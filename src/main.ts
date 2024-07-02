bf21f8e04e1a11e92480ef0dc4fa37ab513f60c8
thejudgeofprogrammers <temka2004@hotmail.com>
2024-07-03 02:59:26 +0300

    docs swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
+
+  const config = new DocumentBuilder()
+    .setTitle("Backend API")
+    .setDescription("Документация по маршрутам в проекте")
+    .setVersion("1.0.0")
+    .addTag("API")
+    .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document) // 1. Prefix (path) 2. Экземпляр приложения 3. Экземпляр документа
   await app.listen(port);
 };
 
